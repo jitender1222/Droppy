fc0f10258fc8f3fb9932e97d8d1ce15a4d4d5cc4
jitender1222 <jeetusinghkanyal@gmail.com>
2025-05-19 11:16:57 +0530

    added SignUp form ui

--- components/signUpForm.tsx
+++ components/signUpForm.tsx
@@ -9,6 +9,25 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Input,
+} from "@heroui/react";
+import {
+  AlertCircle,
+  CheckCircle,
+  Eye,
+  EyeOff,
+  Lock,
+  Mail,
+} from "lucide-react";
+import Link from "next/link";
+
 export const signUpForm = () => {
   const router = useRouter();
   const [verifying, setverifying] = useState(false);
@@ -18,6 +37,8 @@ export const signUpForm = () => {
     null
   );
   const [authError, setAuthError] = useState<string | null>(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const { signUp, isLoaded, setActive } = useSignUp();
 
   const {
@@ -93,8 +114,219 @@ export const signUpForm = () => {
   };
 
   if (verifying) {
-    return <h1>this is OTP entering field</h1>;
+    return (
+      <Card className="w-full max-w-md border border-default-200 bg-default-50 shadow-xl">
+        <CardHeader className="flex flex-col gap-1 items-center pb-2">
+          <h1 className="text-2xl font-bold text-default-900">
+            Verify Your Email
+          </h1>
+          <p className="text-default-500 text-center">
+            We have send a verification Code to your email
+          </p>
+        </CardHeader>
+
+        <Divider />
+
+        <CardBody className="py-6">
+          {verificationError && (
+            <div className="bg-danger-50 text-danger-700 p-4 rounded-lg mb-6 flex items-center gap-2">
+              <AlertCircle className="h-5 w-5  flex-shrink-0" />
+              <p>{verificationError}</p>
+            </div>
+          )}
+
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+            <div className="space-y-2">
+              <label
+                htmlFor="verificationCode"
+                className="text-sm font-medium text-default-900"
+              >
+                Verification Code
+              </label>
+              <Input
+                id="verificationCode"
+                type="text"
+                placeholder="Enter the 6 digit code"
+                value={verificationCode}
+                onChange={(e) => setVerificationCode(e.target.value)}
+                className="w-full"
+                autoFocus
+              />
+            </div>
+
+            <Button
+              type="submit"
+              color="primary"
+              className="w-full"
+              isLoading={isSubmitting}
+            >
+              {isSubmitting ? "Verifying..." : "Verify Email"}
+            </Button>
+          </form>
+
+          <div className="mt-6 text-center">
+            <p className="text-sm text-default-500">
+              Didn't receive a code ?{" "}
+              <button
+                onClick={async () => {
+                  if (signUp) {
+                    await signUp.prepareEmailAddressVerification({
+                      strategy: "email_code",
+                    });
+                  }
+                }}
+                className="text-primary hover:underline font-medium"
+              >
+                Resend Code
+              </button>
+            </p>
+          </div>
+        </CardBody>
+      </Card>
+    );
   }
 
-  return <h1>Signup from with email and other fields</h1>;
+  return (
+    <Card className="w-full max-w-md border border-default-200 bg-default-50 shadow-xl">
+      <CardHeader className="flex flex-col gap-1 items-center pb-2">
+        <h1 className="text-2xl font-bold text-default-900">
+          Create Your Account
+        </h1>
+        <p className="text-default-500 text-center">
+          SignUp to start managing your images securely
+        </p>
+      </CardHeader>
+      <Divider />
+      <CardBody className="py-6">
+        {authError && (
+          <div className="bg-danger-50 text-danger-700 p-4 rounded-lg mb-6 flex items-center gap-2">
+            <AlertCircle className="h-5 w-5" flex-shrink-0 />
+            <p>{authError}</p>
+          </div>
+        )}
+
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-2">
+            <label
+              htmlFor="verificationCode"
+              className="text-sm font-medium text-default-900"
+            >
+              Email
+            </label>
+            <Input
+              id="verificationCode"
+              type="email"
+              startContent={<Mail className="h-4 w-4 text-default-500" />}
+              placeholder="your.email@example.com"
+              isInvalid={!!errors.email}
+              errorMessage={errors.email?.message}
+              {...register("email")}
+              className="w-full"
+            />
+          </div>
+
+          <div className="space-y-2">
+            <label
+              htmlFor="password"
+              className="text-sm font-medium text-default-900"
+            >
+              Password
+            </label>
+            <Input
+              id="password"
+              type={showPassword ? "text" : "password"}
+              placeholder="....."
+              startContent={<Lock className="h-4 w-4 text-default-500" />}
+              endContent={
+                <Button
+                  isIconOnly
+                  variant="light"
+                  size="sm"
+                  onClick={() => setShowPassword(!showPassword)}
+                  type="button"
+                >
+                  {showPassword ? (
+                    <EyeOff className="h-4 w-4 text-default-500 " />
+                  ) : (
+                    <Eye className="h-4 w-4 text-default-500" />
+                  )}
+                </Button>
+              }
+              isInvalid={!!errors.password}
+              errorMessage={errors.password?.message}
+              {...register("password")}
+              className="w-full"
+            />
+          </div>
+
+          <div className="space-y-2">
+            <label
+              htmlFor="passwordConfirmation"
+              className="text-sm font-medium text-default-900"
+            >
+              Confirm Password
+            </label>
+            <Input
+              id="passwordConfirmation"
+              type={showConfirmPassword ? "text" : "password"}
+              placeholder="....."
+              startContent={<Lock className="h-4 w-4 text-default-500" />}
+              endContent={
+                <Button
+                  isIconOnly
+                  variant="light"
+                  size="sm"
+                  onClick={() => setShowPassword(!showPassword)}
+                  type="button"
+                >
+                  {showPassword ? (
+                    <EyeOff className="h-4 w-4 text-default-500 " />
+                  ) : (
+                    <Eye className="h-4 w-4 text-default-500" />
+                  )}
+                </Button>
+              }
+              isInvalid={!!errors.password}
+              errorMessage={errors.password?.message}
+              {...register("passwordConfirmation")}
+              className="w-full"
+            />
+          </div>
+
+          <div className="space-y-4">
+            <div className="flex items-center gap-2">
+              <CheckCircle className="h-5 w-5 text-primary mt-0.5" />
+              <p className="text-sm text-default-600">
+                By Signing up you agree to our Terms of Service and privacy
+                Policy
+              </p>
+            </div>
+          </div>
+
+          <Button
+            type="button"
+            color="primary"
+            className="w-full"
+            isLoading={isSubmitting}
+          >
+            {isSubmitting ? "Creating Account" : "Create Account"}
+          </Button>
+        </form>
+      </CardBody>
+
+      <Divider />
+
+      <CardFooter className="flex justify-center py-4">
+        <p className="text-sm text-default-600">
+          Already have an account?{" "}
+          <Link
+            href="/sign-in"
+            className="text-primary hover:underline font-medium"
+          >
+            Sign in
+          </Link>
+        </p>
+      </CardFooter>
+    </Card>
+  );
 };
