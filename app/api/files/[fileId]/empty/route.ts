a90c07dcfc166dcc7c482a4e8b3335b2ae83c918
jitender1222 <jeetusinghkanyal@gmail.com>
2025-05-24 13:59:40 +0530

    api for mark file as starred

---
+++ app/api/files/[fileId]/empty/route.ts
@@ -0,0 +1,52 @@
+import { db } from "@/lib/db";
+import { files } from "@/lib/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function Patch(
+  request: NextRequest,
+  props: { params: Promise<{ fileId: string }> }
+) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { fileId } = await props.params;
+
+    if (!fileId) {
+      return NextResponse.json(
+        { error: "File Id is required" },
+        { status: 401 }
+      );
+    }
+
+    // mark the file as star
+
+    const [file] = await db
+      .select()
+      .from(files)
+      .where(and(eq(files.id, fileId), eq(files.userId, userId)));
+
+    if (!file) {
+      return NextResponse.json({ error: "File not found" }, { status: 401 });
+    }
+
+    // toggle the mark status
+
+    const updatedFile = db
+      .update(files)
+      .set({ isStarred: !file.isStarred })
+      .where(and(eq(files.id, fileId), eq(files.userId, userId)))
+      .returning();
+
+    console.log("updated file", updatedFile);
+
+    return NextResponse.json(updatedFile);
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to update the file" });
+  }
+}
